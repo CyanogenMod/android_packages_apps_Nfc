ae7d8d800ba73502d21e54d1deef16be0f061866
Martijn Coenen <martijn.coenen@nxp.com>
2010-12-15 00:51:59 +0100

    Multi-tag and multi-protocol clean-up and fixes.
    - The MULTIPLE_TAGS return value from libnfc is now handled:
      in case of multiple tags, we just pick and report the first.
    - Removed all obsolete calls to nfc_jni_get_nfc_tag_handle;
    - Added some safety to CRC checking;
    - NfcService interface now allows connecting to a (different) technology;
    
    Still todo:
    - Get rid of global reference to connected handle;
    - Remove dependency on tech list ordering.
    
    Change-Id: I7176c3c0edf369a8f33f64280dace1c7b1f2b598

--- src/com/android/nfc/NativeNfcTag.java
+++ src/com/android/nfc/NativeNfcTag.java
@@ -32,8 +32,6 @@ import android.util.Log;
 public class NativeNfcTag {
     static final boolean DBG = false;
 
-    private int mHandle;
-
     private int[] mTechList;
     private int[] mTechHandles;
     private Bundle[] mTechExtras;
@@ -182,7 +180,13 @@ public class NativeNfcTag {
     }
 
     public int getHandle() {
-        return mHandle;
+        // This is just a handle for the clients; it can simply use the first
+        // technology handle we have.
+        if (mTechHandles.length > 0) {
+            return mTechHandles[0];
+        } else {
+            return 0;
+        }
     }
 
     public byte[] getUid() {
@@ -197,17 +201,29 @@ public class NativeNfcTag {
         return mTechHandles;
     }
 
+    public int getConnectedHandle() {
+        if (mConnectedTechnology != -1 && mConnectedTechnology < mTechHandles.length) {
+            return mTechHandles[mConnectedTechnology];
+        } else {
+            return 0;
+        }
+    }
     // This method exists to "patch in" the ndef technologies,
     // which is done inside Java instead of the native JNI code.
     // To not create some nasty dependencies on the order on which things
     // are called (most notably getTechExtras()), it needs some additional
     // checking.
-    public void addNdefTechnology(NdefMessage msg, int maxLength, int cardState) {
+    public void addNdefTechnology(NdefMessage msg, int handle, int maxLength, int cardState) {
         synchronized (this) {
             int[] mNewTechList = new int[mTechList.length + 1];
             System.arraycopy(mTechList, 0, mNewTechList, 0, mTechList.length);
             mNewTechList[mTechList.length] = TagTechnology.NDEF;
             mTechList = mNewTechList;
+
+            int[] mNewHandleList = new int[mTechHandles.length + 1];
+            System.arraycopy(mTechHandles, 0, mNewHandleList, 0, mTechHandles.length);
+            mNewHandleList[mTechHandles.length] = handle;
+            mTechHandles = mNewHandleList;
 
             Bundle extras = new Bundle();
             extras.putParcelable(Ndef.EXTRA_NDEF_MSG, msg);
