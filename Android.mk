a907d6f8c670ca07c0b09f3537fa6eecdc6d36b9
Jeff Hamilton <jham@android.com>
2011-08-11 11:49:45 -0500

    Add event logs for NFC events. DO NOT MERGE
    Bug: 5128799
    Change-Id: I5607abe5d28ef9682afa084651c50aa651655b60

--- Android.mk
+++ Android.mk
@@ -3,15 +3,33 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := \
+        $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Nfc
 LOCAL_CERTIFICATE := platform
+
+LOCAL_STATIC_JAVA_LIBRARIES := NfcLogTags
 
 LOCAL_JNI_SHARED_LIBRARIES := libnfc_jni
 
 LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
+
+
+#####
+# static lib for the log tags
+#####
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := src/com/android/nfc/EventLogTags.logtags
+
+LOCAL_MODULE:= NfcLogTags
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
