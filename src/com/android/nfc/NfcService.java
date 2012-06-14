cb0e5a8b51cead15589ee94dba616603d4d160cf
sethyx <sethyx@gmail.com>
2012-06-15 00:54:19 +0200

    NFC polling mode (1/3)
    Change-Id: I0ef68ecb0de671aa085ab7b0d3f206ba619aea1c
    Signed-off-by: sethyx <sethyx@gmail.com>

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -43,6 +43,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources.NotFoundException;
+import android.database.ContentObserver;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.net.Uri;
@@ -157,7 +158,7 @@ public class NfcService implements DeviceHostListener {
     static final int EE_ERROR_NFC_DISABLED = -6;
 
     /** minimum screen state that enables NFC polling (discovery) */
-    static final int POLLING_MODE = SCREEN_STATE_ON_UNLOCKED;
+    static int POLLING_MODE = SCREEN_STATE_ON_UNLOCKED;
 
     // Time to wait for NFC controller to initialize before watchdog
     // goes off. This time is chosen large, because firmware download
@@ -531,6 +532,8 @@ public class NfcService implements DeviceHostListener {
         }
 
         new EnableDisableTask().execute(TASK_BOOT);  // do blocking boot tasks
+
+        SettingsObserver mSettingsObserver = new SettingsObserver(new Handler());
     }
 
     void initSoundPool() {
@@ -2561,6 +2564,29 @@ public class NfcService implements DeviceHostListener {
             mNfcDispatcher.dump(fd, pw, args);
             pw.println(mDeviceHost.dump());
 
+        }
+    }
+
+    protected class SettingsObserver extends ContentObserver {
+        ContentResolver resolver;
+        SettingsObserver(Handler handler) {
+            super(handler);
+            resolver = mContext.getContentResolver();
+            observe();
+        }
+
+        void observe() {
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.NFC_POLLING_MODE),
+                    false, this);
+            POLLING_MODE = Settings.System.getInt(resolver,
+                    Settings.System.NFC_POLLING_MODE, SCREEN_STATE_ON_UNLOCKED);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            POLLING_MODE = Settings.System.getInt(resolver,
+                    Settings.System.NFC_POLLING_MODE, SCREEN_STATE_ON_UNLOCKED);
         }
     }
 }
