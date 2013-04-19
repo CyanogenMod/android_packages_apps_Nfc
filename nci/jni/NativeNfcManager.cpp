9cc194fd111bd41013b2ba1b096b0111ae7585fb
Martijn Coenen <maco@google.com>
2013-04-19 09:29:21 -0700

    Remove special deactivation handling code.
    Original purpose of this block is unclear, so
    removing it.
    
    Change-Id: Ic1753f3a8c7a4d688b2f31fb6184ec6131a5ffe8

--- nci/jni/NativeNfcManager.cpp
+++ nci/jni/NativeNfcManager.cpp
@@ -210,20 +210,6 @@ static void nfaConnectionCallback (UINT8 connEvent, tNFA_CONN_EVT_DATA* eventDat
     tNFA_STATUS status = NFA_STATUS_FAILED;
     ALOGD("%s: event= %u", __FUNCTION__, connEvent);
 
-    // TODO this if can probably be completely removed. It's unclear why this
-    // was present in the initial code drop - either to work around NFCC,
-    // stack or certain NFC tags bugs. Until we verify removing it doesn't
-    // break things, leave it be.
-    if (gIsTagDeactivating && connEvent != NFA_DEACTIVATED_EVT &&
-            connEvent != NFA_PRESENCE_CHECK_EVT && connEvent != NFA_DATA_EVT &&
-            connEvent != NFA_RW_INTF_ERROR_EVT)
-    {
-        // special case to switching frame interface for ISO_DEP tags
-        gIsTagDeactivating = false;
-        ALOGD("%s: deactivating, should get NFA_DEACTIVATED_EVT", __FUNCTION__);
-        nativeNfcTag_doDeactivateStatus(1);
-    }
-
     switch (connEvent)
     {
     case NFA_POLL_ENABLED_EVT: // whether polling successfully started
