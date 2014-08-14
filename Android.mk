73fa3d8e3d049718947ad26d339a85f566a90bf8
Martijn Coenen <maco@google.com>
2014-08-14 12:17:32 -0700

    Remove deprecated event logging.
    Bug: 17033091
    Change-Id: I3a096699339f4c150e14af5fdf34835653a571c3

--- Android.mk
+++ Android.mk
@@ -49,18 +49,4 @@ LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
 
-#####
-# static lib for the log tags
-#####
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SRC_FILES := src/com/android/nfc/EventLogTags.logtags
-
-LOCAL_MODULE:= NfcLogTags
-
-include $(BUILD_STATIC_JAVA_LIBRARY)
-
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
